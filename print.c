acb7785883b0ad336d5f591a694b13927c957925
Vinayakchoukse1 <80781272+Vinayakchoukse1@users.noreply.github.com>
2021-03-17 15:07:03 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"functions.h"
+void print()
+{
+	printf("Hello my name is Vinayak Choukse dfndkdk     \n");
+}
+
---
+++ prog2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"functions.h"
+void print(void);
+void sum(int a, int b);
+
+int main()
+{
+	int a=3,b=6;
+	print();
+	sum(a,b);
+}
+
